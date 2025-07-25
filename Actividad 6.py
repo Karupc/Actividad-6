1ff3f7792e225025218f8d8f981d4109a0a77b0b
Karup <kperca802@gmail.com>
2025-07-24 19:04:24 -0600

    Ingresar números y mostrar

--- Actividad 6.py
+++ Actividad 6.py
@@ -1,5 +1,5 @@
 numeros = []
-print("\n--MENÚ DE OPERACIONES MAFTEMÁTICAS--")
+print("\n--MENÚ DE OPERACIONES MAFTEMÁTICAS--\n")
 print("1.- Ingresar números y mostrar\n"
       "2.- Calcular el área de un triángulo\n"
       "3.- Verificar si un número es par o impar\n"
@@ -11,8 +11,37 @@ match opciones:
     case "1":
         cantidad = int(input("¿Cuántos números desea ingresar?: "))
         for i in range(cantidad):
-            numero = input(f"Ingrese el la cantidad {i+1}: ")
+            numero = int(input(f"Ingrese el la cantidad {i+1}: "))
             numeros.append(numero)
-        
-
+        while True:
+            print("\n--ELIJA LO QUE DESEA VER--\n"
+                  "1.- La suma total\n"
+                  "2.- El promedio\n"
+                  "3.- La cantidad de números positivos y negativos\n"
+                  "4.- Salir")
+            opciones2 = input("Escriba el número de la opción que elige: ")
+            match opciones2:
+                case "1":
+                    def suma_total():
+                        suma = sum(numeros)
+                        print(f"La suma total es: {suma}")
+                    suma_total()
+                case "2":
+                    def promedio():
+                        prom = sum(numeros) / len(numeros)
+                        print(f"El promedio es: {prom}")
+                    promedio()
+                case "3":
+                    positivos = 0
+                    negativos = 0
+                    for num in numeros:
+                        if num > 0:
+                            positivos += 1
+                        elif num < 0:
+                            negativos += 1
+                    print(f"Cantidad de números positivos: {positivos}")
+                    print(f"Cantidad de números negativos: {negativos}")
+                case "4":
+                    print("Volverá al primer menú")
+                    break
 
